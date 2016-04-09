84696a10c554debf31e6426074a5d6ec35e15987
Sambriddhi Mainali <smainali@memphis.edu>
2016-04-09 03:20:18 +0000

    event details page changed

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   resources :events
   resources :locations
   resources :keywords
-
+  resources :pages
   get 'Foodie', to: 'pages#home', as: 'Foodie'
+  root to: 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
