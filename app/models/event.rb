0b3cd0ec36dcdba502e3453544f9f60d1480d037
Sambriddhi Mainali <smainali@memphis.edu>
2016-04-10 22:00:57 +0000

    edit event form changed

--- app/models/event.rb
+++ app/models/event.rb
@@ -29,7 +29,7 @@ class Event < ActiveRecord::Base
 	belongs_to :user
 	has_one :location
 	has_and_belongs_to_many :keywords
-
+	accepts_nested_attributes_for :location
 	def event_date_is_valid_date
     	if !event_date.is_a?(Date)
       		errors.add(:event_date, 'must be a valid date') 
