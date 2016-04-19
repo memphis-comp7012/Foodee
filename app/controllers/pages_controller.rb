c06e3b3cc60f75193de621f3adbe9503311d1cbf
tabedse <tabedse@memphis.edu>
2016-04-19 22:09:55 +0000

    Added pagination to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,9 +3,11 @@ class PagesController < ApplicationController
 	before_action :authenticate_user!, only: [:home]
 
 	def home
-    	@events = Event.all
+    	#@events = Event.all
+    	@events = Event.all.paginate(:page => params[:page], :per_page => 2)
     	if current_user
     		@person = current_user.person
+
     	end
   	end
 end
