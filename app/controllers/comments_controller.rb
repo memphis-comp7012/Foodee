27e8471d49679265b04bad36c4d18cc069648401
tabedse <tabedse@memphis.edu>
2016-05-04 12:22:30 +0000

    comment fixed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -82,6 +82,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:comment, :score)
+      params.require(:comment).permit(:comment, :score, :event_id, :person_id)
     end
 end
